347c33cc77bf6dd77cac4dc10cb0593b2789f4fe
Nathan <nbel806@aucklanduni.ac.nz>
2022-07-08 00:19:50 +1200

    New Class

--- src/Accounts.java
+++ src/Accounts.java
@@ -1,4 +1,20 @@
 
 public class Accounts {
+	private String accName;
+	private String accPass;
+	private double accBalance;
 
+	public Accounts(String Name, String Pass) {
+		accName = Name;
+		accPass = Pass;
+		accBalance = 0;
+	}
+
+	public double getBalance() {
+		return accBalance;
+	}
+
+	public String getAccPass() {
+		return accPass;
+	}
 }
