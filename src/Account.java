c18cfbdfde60d4b85a4b461bb1bd44653e07963b
Nathan <nbel806@aucklanduni.ac.nz>
2022-07-07 23:16:42 +1200

    First Commit

---
+++ src/Account.java
@@ -0,0 +1,13 @@
+
+public class Account {
+	private int accountBallance;
+	private String accountName;
+	private String accountPassword;
+
+	public Account(String accName, String accPass) { // Constructor
+		accountName = accName;
+		accountPassword = accPass;
+		accountBallance = 0;
+
+	}
+}
