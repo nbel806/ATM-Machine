529f23bf2086a8b0f4288d8d8e4978743c17d483
Nathan <nbel806@aucklanduni.ac.nz>
2022-07-08 19:15:15 +1200

    Added Transfer

--- src/ATM.java
+++ src/ATM.java
@@ -42,18 +42,18 @@ public class ATM {
 			System.out.println("Not logged in");
 			return;
 		} else {
-			System.out.println("Balance is $" + accBalance.get(loggedInIndex));// returns balance
+			System.out.println("Current balance is $" + accBalance.get(loggedInIndex));// returns balance
 		}
 	}
 
-	public void withdraw(float amount) {
+	public void withdraw(float amount) {// takes out amount entered
 		if (loggedInIndex == -1) { // checks its not logged out
 			System.out.println("Not logged in");
 			return;
 		} else {
 			if (accBalance.get(loggedInIndex) >= amount) {
 				System.out.println("Withdrew $" + amount);
-				accBalance.set(loggedInIndex, (accBalance.get(loggedInIndex) - amount));
+				accBalance.set(loggedInIndex, (accBalance.get(loggedInIndex) - amount));// removes amount from balance
 				this.Balance();
 			} else {
 				System.out.println("Balance not enough");
@@ -62,16 +62,37 @@ public class ATM {
 
 	}
 
-	public void deposit(float amount) {
+	public void deposit(float amount) {// deposits amount entered
 		if (loggedInIndex == -1) { // checks its not logged out
 			System.out.println("Not logged in");
 			return;
 		} else {
 			System.out.println("Depostied $" + amount);
-			accBalance.set(loggedInIndex, (accBalance.get(loggedInIndex) + amount));
+			accBalance.set(loggedInIndex, (accBalance.get(loggedInIndex) + amount));// adds amount to balance
 			this.Balance();
 		}
 
+	}
+
+	public void transfer(float amount, String accName) {// shifts amount from logged ijn acc to desired acc
+		int moneyToIndex;
+		if (accNames.contains(accName)) {
+			moneyToIndex = accNames.indexOf(accName);
+		} else {
+			System.out.println("UserName doesnt exsist");// Account doesn't exist so will break for new name
+			loggedInIndex = -1;
+			return;
+		}
+		if (accBalance.get(loggedInIndex) >= amount) {
+			accBalance.set(loggedInIndex, (accBalance.get(loggedInIndex) - amount));// removes amount from sending acc
+			accBalance.set(moneyToIndex, (accBalance.get(moneyToIndex) + amount));// adds amount to desired acc
+			System.out.println("Transfered $" + amount);
+			this.Balance();
+		} else {
+			System.out.println("Could not transfer, not enough money in account");
+			System.out.println("Deposit money and try again");
+			this.Balance();
+		}
 	}
 
 }
